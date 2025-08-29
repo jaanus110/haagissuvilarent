d5f362a1f3a50346c5c7036592da94fbd38cbfc0
Jaanus <jaanus1@hotmail.com>
2025-08-29 11:47:03 +0300

    Deleted easyAppointmnets!

--- caravan-rental-website/calendar.php
+++
@@ -1,6 +0,0 @@
-<!-- Easy!Appointments Booking Embed -->
-<iframe 
-    src="https://tartuhaagissuvila.ee/easyappointments/index.php/booking" 
-    style="width: 100%; height: 900px; border: none;" 
-    title="Caravan Rental Booking">
-</iframe>
